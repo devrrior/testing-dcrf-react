5973041ba508697967f891f39de3facbe969bd23
Fernando Guerrero <devrrior@gmail.com>
2022-03-01 21:48:00 -0600

    set up webpack and styles

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,29 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-react',
-                {
-                  runtime: 'automatic', // default 'classic'
-                },
-              ],
-            ],
+const rulesForStyles = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+};
+
+const rulesForJavaScript = {
+  test: /\.m?js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            runtime: 'automatic', // default 'classic'
           },
-        },
-      },
-    ],
+        ],
+      ],
+    },
   },
+};
+
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProduction = mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'main.js',
+      path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+    module: {
+      rules: [rulesForJavaScript, rulesForStyles],
+    },
+    devServer: {
+      open: true,
+      port: 3000,
+    },
+  };
 };
