22d974f3c0136a9a10539d3a981b661965bb76d9
Fernando Guerrero <devrrior@gmail.com>
2022-03-04 20:11:48 -0600

    prepare front for api

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,31 +1,38 @@
 import { useEffect, useState } from 'react';
 
 const ChatPage = () => {
-  const [message, setMessage] = useState('');
   const [url, setUrl] = useState(`ws://127.0.0.1:8000/ws/socket-server/`);
   const [chatSocket, setChatSocket] = useState(new WebSocket(url));
+  const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    getWebSocket();
-  }, []);
+    chatSocket.onopen = (e) => {
+      fetchMessages();
+    };
 
-  useEffect(() => {
-  }, [messages]);
-
-  const getWebSocket = () => {
     chatSocket.onmessage = function (e) {
       const data = JSON.parse(e.data);
 
-      if (data.type === 'chat') {
-        const conMessage = [...messages, data.message]
-        console.log(typeof conMessage)
-        console.log('messages', conMessage)
-        // setMessages([...conMessage, data.message]);
-        // console.log('messages:', messages);
-
+      if (data['command'] === 'messages') {
+        setMessages(data['messages']);
+        for (let i = 0; i < data['messages'].length; i++) {
+          console.log(data['messages'][i]);
+        }
+      } else if (data['command'] === 'new_message') {
+        console.log(data['message']);
       }
     };
+
+    // chatSocket.onclose = function (e) {
+    //   console.error('Chat socket closed unexpectedly');
+    // };
+  }, []);
+
+  useEffect(() => {}, [messages]);
+
+  const fetchMessages = () => {
+    chatSocket.send(JSON.stringify({ command: 'fetch_messages' }));
   };
 
   const handleChange = (event) => {
@@ -37,6 +44,7 @@ const ChatPage = () => {
     chatSocket.send(
       JSON.stringify({
         message: message,
+        command: 'new_message',
       })
     );
     document.querySelector('#form').reset();
@@ -44,23 +52,30 @@ const ChatPage = () => {
 
   return (
     <>
-      <div className='container bg-slate-300'>
+      <div className='container bg-slate-300 mx-auto my-10 rounded-lg h-1/2'>
         {/* <textarea rows={10} cols={40}></textarea> */}
         <form id='form' onSubmit={handleSumit}>
           <input
             type={'text'}
             name='message'
             placeholder='type something'
+            className='mx-5'
             onChange={handleChange}
           />
-          <button type='submit' className='bg-blue-50 p-2 rounded-lg block'>
+          <button type='submit' className='bg-blue-50 my-5 p-2 rounded-lg'>
             Send
           </button>
         </form>
 
-        <div className='messages'>
-          {messages.map((item, key) => {
-            return(<div key={key}>{item}</div>)
+        <div className='messages ml-5 w-1/4'>
+          <h3 className='font-bold'>Messages:</h3>
+          {messages.map((message, key) => {
+            return (
+              <div key={key} className='min-h-full'>
+                {message['content']} - by {message['author']} (
+                {message['timestamp']})
+              </div>
+            );
           })}
         </div>
       </div>
