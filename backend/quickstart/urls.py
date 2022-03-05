2a2c897d72fe4a0d90efbc81bdb5bf59f5206f78
Fernando Guerrero <devrrior@gmail.com>
2022-03-05 12:56:17 -0600

    add simplejwt and listAPIView

--- backend/quickstart/urls.py
+++ backend/quickstart/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from .views import getRoutes
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import getRoutes, MyTokenObtainPairView, ListMessagesAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-	# path('', UserApiView.as_view(), name='userapiview'),
+    # path('', UserApiView.as_view(), name='userapiview'),
     path('', getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('messages/', ListMessagesAPIView.as_view(), name='list_messages'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/quickstart/views.py
+++ backend/quickstart/views.py
@@ -1,6 +1,14 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 # from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view, permission_classes
+from chat.models import Message
+
+from quickstart.serializer import MessageSerializer
 
 # class UserApiView(APIView):
 #     def get(self, request):
@@ -11,8 +19,30 @@ from rest_framework.decorators import api_view
 #         return Response(person)
 @api_view(['GET'])
 def getRoutes(request):
-    routes = [
-        '/api/token',
-        '/api/token/refresh'
-    ]
+    routes = ['/api/token', '/api/token/refresh']
     return Response(routes)
+
+
+class ListMessagesAPIView(ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated,]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.author_messages.all()
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
